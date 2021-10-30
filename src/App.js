51640fd7cb777245a00637cac4f061ee737ca911
PrathamMittal17 <prathammittal172002@gmail.com>
2021-10-30 14:47:45 +0530

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { createContext, useState, useEffect } from 'react';
 import Navigation from './Components/NavigationBar/Navigation'
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import {BrowserRouter,Route,Switch,HashRouter} from 'react-router-dom';
 import ProductCardList from './Components/ProductCard/ProductCardList';
 import Cart from './Components/Cart/Cart';
 import Profile from './Components/Profile/Profile';
@@ -66,7 +66,7 @@ useEffect(() => {
     LoginForm=<SignIn/>
   }
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
      
      
     <Switch>
@@ -106,7 +106,7 @@ useEffect(() => {
         
 
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
